fb26e227a78094b04bb33eece8baa4bbd7906831
uptown <uptown@linuxremote1.bucknell.edu>
2020-04-03 15:32:20 -0400

    fixed feature where some text would get lopped off of attachment name when sending email

--- emailHelper.py
+++ emailHelper.py
@@ -42,7 +42,7 @@ def sendEmail(emailfrom, emailto, fileToSend, eventName, username, password):
         attachment.set_payload(fp.read())
         fp.close()
         encoders.encode_base64(attachment)
-    attachment.add_header("Content-Disposition", "attachment", filename=fileToSend[-13:])
+    attachment.add_header("Content-Disposition", "attachment", filename=fileToSend)
     msg.attach(attachment)
 
     s = smtplib.SMTP('smtp.gmail.com:587')
